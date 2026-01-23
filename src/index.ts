8b8011e980862ed17c03b2b7198ec9e03c3ae0e3
AASHRITH <aashrith@Mac.lan>
2026-01-23 22:32:23 +0530

    Better-Auth Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import subjectsRouter from './routes/subjects';
+import usersRouter from './routes/users';
 import cors from 'cors';
 import securityMiddleware from "./middleware/security";
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "./lib/auth";
 
 const app = express();
 const port = 8000;
@@ -10,13 +13,17 @@ app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials:true,
     methods:['GET','POST','PUT','DELETE']
-}))
+}));
+
+app.all('/api/auth/*splat',toNodeHandler(auth));
+
 app.use(express.json());
 
 app.use(securityMiddleware);
 
 
 app.use('/api/subjects',subjectsRouter)
+app.use('/api/users',usersRouter)
 
 app.get('/',(req,res) => {
     res.send("Hello World!");
